8a76a288a7e3a808f3e738dfbb88f2ffd9abf500
Charlotte Rouby <charlotte.rouby@hotmail.fr>
2018-08-30 18:09:00 +0200

    dynaformLib : ajout composant formButton

---
+++ projects/dynaform-lib/src/lib/components/form-button/form-button.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormButton } from '../../models/form-button.model';
+
+@Component({
+	selector: 'dfl-form-button',
+	templateUrl: './form-button.component.html',
+	styleUrls: ['./form-button.component.css']
+})
+export class FormButtonComponent implements OnInit {
+
+	config: FormButton;
+	group: FormGroup;
+	buttonColor = 'primary';
+
+	constructor() {
+
+	}
+
+	ngOnInit() {
+		this.buttonColor = this.config.inputType === 'submit' ? 'accent' : 'primary';
+
+	}
+
+}
